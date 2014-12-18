4f43740f34c78ee9c9d815ab473e129451cc16d3
grakus06 <claude.marescot@gmail.com>
2014-12-18 21:04:10 +0000

    force asset precompilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,9 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # set to true to have a recompil forced after bootstrap integration
+  # to have it work under heroku
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
